afa706c91e7a7ee77e237ccc8d3f611418b3a359
edenlewis <edenlewis80@gmail.com>
2023-12-01 18:40:10 -0700

    fixed my tests

--- libs/shapes.js
+++ libs/shapes.js
@@ -8,17 +8,17 @@ class Shape {
 }
 class Triangle extends Shape{
     render(){
-        return'<polygon points="150, 18 244, 182 56, 182" fill=${this.color}/>'
+        return`<polygon points="150, 18 244, 182 56, 182" fill="${this.color}"/>`
     }
 }
 class Circle extends Shape{
   render(){
-      return`<circle cx="200" cy="200" r="180" fill=${this.color}/>`
+      return`<circle cx="200" cy="200" r="180" fill="${this.color}"/>`
   }
 }
 class Square extends Shape{
   render(){
-      return'<rect x="50" y="20" width="300" height="300" fill=${this.color}/>'
+      return`<rect x="50" y="20" width="300" height="300" fill="${this.color}"/>`
   }
 }
 module.exports = {Triangle, Circle, Square}
--- libs/shapes.test.js
+++ libs/shapes.test.js
@@ -1,9 +1,30 @@
-const triangle = new Triangle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"/>');
-const circle = new Circle();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<circle="cx="50" cy="50" r="40" fill="blue"/>');
-const square = new Square();
-shape.setColor("blue");
-expect(shape.render()).toEqual('<rect x="50" y="20" width="150" height="150" fill="blue"/>');
+const { Triangle, Circle, Square } = require("./shapes");
+
+describe("Test the Triangle class", () => {
+  test("will this render the shape element", () => {
+    const triangle = new Triangle();
+    triangle.setColor("blue");
+    expect(triangle.render()).toEqual(
+      '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>'
+    );
+  });
+});
+describe("Test the Triangle class", () => {
+  test("will this render the shape element", () => {
+    const square = new Square();
+    square.setColor("blue");
+    expect(square.render()).toEqual(
+      '<rect x="50" y="20" width="300" height="300" fill="blue"/>'
+    );
+  });
+});
+describe("Test the Circle class", () => {
+  test("will this render the shape element", () => {
+    const circle = new Circle();
+    circle.setColor("blue");
+    expect(circle.render()).toEqual(
+      '<circle cx="200" cy="200" r="180" fill="blue"/>'
+    );
+  });
+});
+// const square = new Square();
